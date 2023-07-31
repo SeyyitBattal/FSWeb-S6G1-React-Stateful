34f4a4003c92fa7a8f0dbbeb7afa44a2b350b999
SeyyitBattal <seyyitbattalarvas3@gmail.com>
2023-07-31 17:09:26 +0300

    sayici.js is finished

--- src/components/Sayac.js
+++ src/components/Sayac.js
@@ -14,68 +14,88 @@ Saf bir geliştirici 3 farklı state dilimi diyebilir:
 Ancak burada sadece bir state gereklidir: sayici!
 Diğer şeyler basitçe sayımın kendisinden türetilebilir.
 
-ADIM 0:
-  Aşağıdaki bileşeni inceleyerek ve state hookunu import ederek başlayın.
+ADIM 0: +
+  Aşağıdaki bileşeni inceleyerek ve state hookunu import ederek başlayın. 
 
-ADIM 1:
+ADIM 1: +
   State hookunu kullanara, bir 'sayici', 'setSayici' çifti oluşturun.
   'sayici' state'inin ilk değeri 0 olarak yüklenmelidir.
 
-ADIM 2:
+ADIM 2: +
   'stil' nesnesinde 'color' niteliği sabit olarak "royalblue" olarak belirlenmiştir.
   Bunun yerine 'color' değerinin ne olması gerektiği, şu şekilde belirlenmelidir:
   Eğer sayici çiftse "royalblue", değilse "crimson" olmalıdır.
 
-ADIM 3:
+ADIM 3: +
   JSX'teki bazı sabit kodlanmış bilgileri, kıvrımlı parantezler ({}) içinde enterpolasyonlu ifadelerle değiştirmemiz gerekiyor.
   Sabit olarak yazılmış "0" sayısını {sayici} değişkeni ile değiştirerek başlayın.
   Sonra "çift" kelimesini şu kurala göre değiştirin: {eğer sayıcı çiftse, kelime `çift` değilse `tek` olacak}.
 
-ADIM 4:
+ADIM 4: +
   Bu click handler'ın, "sayici" stateini artı bir yapacak şekilde programlamak için "setSayici"i kullanması gerekiyor.
   Bu state değişiklikleri senkronize değil: güncellenen sayım, Sayac bileşeninin bir sonraki çalıştırmasında da gelir.
   Sakın basitçe sayici++ yapmayın. Artı artı yasak! Bu tarz artırım bir sonraki çağırımda gelmez. Her zaman state'e yeni değer 
   atamak için state güncelleyici kullanmalısınız.
 
-ADIM 5:
+ADIM 5: +
   Bu click handler 'sayici' yi bir azaltmak için 'setSayici' ı kulanacak.
   sayici-- SAKIN kullanmayın. Bu tarz kullanım bir sonraki çalıştırmada gelmez.
 
-ADIM 6:
+ADIM 6: +
 	Bu click handler 'sayici' yı sıfırlamak için 'setSayici' yi kullanacak.
 */
 
-import React from 'react'; /* ADIM 0 buraya*/
+import React, { useState } from "react"; /* ADIM 0 buraya*/
 
 export default function Sayac() {
   /* ADIM 1 buraya*/
-	
-	
+  const [sayici, setSayici] = useState(0);
+  let renk;
+  let metin;
+
+  // !(sayici % 2) ? (renk = "royalblue") : (renk = "crimson");
+
+  if (sayici % 2 === 0) {
+    renk = "royalblue";
+    metin = "even";
+  } else {
+    renk = "crimson";
+    metin = "odd";
+  }
+
   const artirici = () => {
-    /* ADIM 4 buraya */
+    setSayici(sayici + 1);
   };
   const azaltici = () => {
     /* ADIM 5 */
+    setSayici(sayici - 1);
   };
   const reset = () => {
+    setSayici(0);
   };
 
   const stil = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: 'royalblue', /* ADIM 2 */
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: renk /* ADIM 2 */,
   };
 
   return (
-    <div className='widget-counter container'>
+    <div className="widget-counter container">
       <h2>Sayaç</h2>
-      <div id='sayici' style={stil}>
-        Sayı {sayici} {/* ADIM 3  buraya*/ }
+      <div id="sayici" style={stil}>
+        Number {sayici} is {metin}
       </div>
       <div>
-        <button id='artirici' onClick={artirici}>Artırıcı</button>
-        <button id='decrement' onClick={azaltici}>Azaltıcı</button>
-        <button id='resetCount' onClick={reset}>Reset</button>
+        <button id="artirici" onClick={artirici}>
+          Artırıcı
+        </button>
+        <button id="decrement" onClick={azaltici}>
+          Azaltıcı
+        </button>
+        <button id="resetCount" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
